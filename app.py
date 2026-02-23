d9195926e5ebb3b2cfc7d6832cd4f60533c4190d
Sojourner <aryasandilya41@gmail.com>
2026-02-23 11:40:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Travel Booking App, AirBNB !"
+    return "Welcome to Travel Booking App, AirBNB and OYO!"
 
 @app.route('/hotels')
 def hotels():
